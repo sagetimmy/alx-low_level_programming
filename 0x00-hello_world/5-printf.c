b70bf97b9e78abcd2e1d79510a5fac645da86098
Timothy <ngetichtimothy05@gmail.com>
2023-06-18 07:24:13 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,17 @@
 #!/bin/bash
 #include <stdio.h>
 
-int main() {
-    printf("I am a piece of art,");
-    printf(" but I am not a masterpiece.\n");
 
-    return 0;
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
-
