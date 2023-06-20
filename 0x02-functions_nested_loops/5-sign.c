d95eccfe0d3b4fd7b455e55fc6dbc19182cf0338
Timothy <ngetichtimothy05@gmail.com>
2023-06-20 06:32:35 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 /**
  * print_sign - Prints the sign of a number
@@ -10,18 +11,58 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-");
+		putchar('-');
 		return (-1);
 	}
 	else
 	{
-		printf("0");
+		putchar('0');
 		return (0);
 	}
+}
+
+int main(void)
+{
+	int result;
+
+	/* Test cases with expected correct outputs */
+	result = print_sign(98);
+	printf("Expected output: +\n");
+	printf("Actual output  : %c\n", result);
+
+	result = print_sign(0);
+	printf("Expected output: 0\n");
+	printf("Actual output  : %c\n", result);
+
+	result = print_sign(0xff);
+	printf("Expected output: +\n");
+	printf("Actual output  : %c\n", result);
+
+	result = print_sign(-1);
+	printf("Expected output: -\n");
+	printf("Actual output  : %c\n", result);
+
+	result = print_sign(99);
+	printf("Expected output: +\n");
+	printf("Actual output  : %c\n", result);
+
+	result = print_sign(INT_MAX);
+	printf("Expected output: +\n");
+	printf("Actual output  : %c\n", result);
+
+	result = print_sign(INT_MIN);
+	printf("Expected output: -\n");
+	printf("Actual output  : %c\n", result);
+
+	result = print_sign(-4);
+	printf("Expected output: -\n");
+	printf("Actual output  : %c\n", result);
+
+	return 0;
 }
 
