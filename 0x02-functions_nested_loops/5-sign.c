6bdd00728ca32f1ee4a7e1e30101a7ab64c45f61
Timothy <ngetichtimothy05@gmail.com>
2023-06-21 04:44:17 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,68 +1,30 @@
-#include <stdio.h>
-#include <limits.h>
+#include"main.h"
 
 /**
- * print_sign - Prints the sign of a number
- * @n: The number to be checked
+ * print_sign - print + if n is greater than zero,
+ *             0 if n is zero and - if n is less
+ *             than zero.
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
- */
+ * @n: takes integer type input for function.
+ *
+ * Return: 1 if +, 0 if 0 and -1 if -
+*/
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
-		return 1;
+		_putchar(43 + 0);
+		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		putchar('-');
-		return -1;
+		_putchar(0 + 48);
+		return (0);
 	}
 	else
 	{
-		putchar('0');
-		return 0;
+		_putchar(45 + 0);
+		return (-1);
 	}
 }
-
-int main(void)
-{
-	int result;
-
-	/* Test cases with expected correct outputs */
-	result = print_sign(98);
-	printf("Expected output: +\n");
-	printf("Actual output  : %c\n", result > 0 ? '+' : result == 0 ? '0' : '-');
-
-	result = print_sign(0);
-	printf("Expected output: 0\n");
-	printf("Actual output  : %c\n", result > 0 ? '+' : result == 0 ? '0' : '-');
-
-	result = print_sign(0xff);
-	printf("Expected output: +\n");
-	printf("Actual output  : %c\n", result > 0 ? '+' : result == 0 ? '0' : '-');
-
-	result = print_sign(-1);
-	printf("Expected output: -\n");
-	printf("Actual output  : %c\n", result > 0 ? '+' : result == 0 ? '0' : '-');
-
-	result = print_sign(99);
-	printf("Expected output: +\n");
-	printf("Actual output  : %c\n", result > 0 ? '+' : result == 0 ? '0' : '-');
-
-	result = print_sign(INT_MAX);
-	printf("Expected output: +\n");
-	printf("Actual output  : %c\n", result > 0 ? '+' : result == 0 ? '0' : '-');
-
-	result = print_sign(INT_MIN);
-	printf("Expected output: -\n");
-	printf("Actual output  : %c\n", result > 0 ? '+' : result == 0 ? '0' : '-');
-
-	result = print_sign(-4);
-	printf("Expected output: -\n");
-	printf("Actual output  : %c\n", result > 0 ? '+' : result == 0 ? '0' : '-');
-
-	return 0;
-}
-
